985768bce2b338e23c44cc90dff681bd81f9c584
SynapticRetouch <Synapticemail@gmail.com>
2024-03-06 21:35:07 -0800

    Provider str fix

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
 import code_demo
  
 if __name__ == "__main__":
-    #code_demo.exercise_records()
-    code_demo.sample_load_member(101195901)
+    code_demo.exercise_records()
+    # code_demo.sample_load_member(101195901)
