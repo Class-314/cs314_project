744476c4b2ab5bd9259b60626a692737b25bf865
SamNelson44 <sn22@pdx.edu>
2024-03-10 23:34:28 -0700

    .

--- Reporter.py
+++ Reporter.py
@@ -1,20 +1,44 @@
+from DataBaseManager import *
+
 
 class Reporter:
     def __init__(self) -> None:
         self._service_report_file = None
 
-    def write_weekly_report():
-        return
-    
+
     def write_member_report():
+        #get member info from member record
+
+        #get provider and services provided to member
+
+        #write in order of date of service provided
+
         return
-    
+
+
     def write_provider_report():
+        #get provider infor from provider record
+
+        #get service info with member name and number
+
+        #get total number of consultations with members
+
+        #get total fee for the week 
         return
-    
+
+
     def write_summary_report():
+        #get every provider that provided service for the week
+            #get number of consultations each provider had
+            #get total fee for the week per provider
+        
+        #get total number of providers who provided services
+        #get total number of consultations, and the overall fee total are printed.
+
         return
-    
+
+
     def write_eft_data():
+
         return
     
